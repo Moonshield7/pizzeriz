25297c55b54856cd76b473af4138b51de08fbbf9
pirtah <99216086+pirtah@users.noreply.github.com>
2022-04-19 14:41:17 +0200

    Traitement formulaire inscription

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RegisterType extends AbstractType
